a8ccf242c77a0d079c2167b05befdfa18a6ed625
SARAGADAM KIRAN <76104406+KiranSaragadam@users.noreply.github.com>
2021-06-01 06:03:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ Is_Activeclf = pickle.load(open('Is_Active', 'rb'))
 Credit_Productclf = pickle.load(open('Credit_Product', 'rb'))
 Channel_Codeclf = pickle.load(open('Channel_Code', 'rb'))
 model = pickle.load(open('model', 'rb'))
-
+feature_importance = pickle.load(open('feature_importance', 'rb'))
+feature = pickle.load(open('feature', 'rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -20,23 +21,10 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-  #  Occupation={'0':'Entrepreneur','1':'Self_Employed','2':'Other','3':'Salaried'}
-  #  Channel_Code={'0':'X1','1':'X2','2':'X3','3':'X4'}
-  #  Credit_Product={'0':'YES','1':'NO','2':'NA'}
-  #  Is_Active={'0':'YES','1':'NO'}
-    
+   
     final_features=[]
     input=request.form.to_dict()
     value = list(input.values())
-   # if(value[0]=='0'):
-    #    value[0]='Male'
-   # else:
-     #   value[0]='Female'
-   # value[2]='RG'+str(250+int(value[2]))
-#    value[3]=Occupation[value[3]]
-#    value[4]=Channel_Code[value[4]]
-#    value[6]=Credit_Product[value[6]]
-#    value[8]=Is_Active[value[8]]
     final_features.extend(list(genderclf.transform([value[0]]).toarray()[0]))
     final_features.append(int(value[1]))
     final_features.extend(list(Region_Codeclf.transform([value[2]]).toarray()[0]))
@@ -53,8 +41,11 @@ def predict():
         output='Yes'
     else:
         output='No'
-
-    return render_template("home.html", prediction = output)
+    proba=model.predict_proba(final_features)
+    class1=str(proba[0][max(prediction[0],1)])
+    class0=str(proba[0][min(prediction[0],0)])
+    imp=np.array(feature)[np.where(final_features[0]>=1)][np.argsort(feature_importance[np.where(final_features[0]>=1)])[::-1]]
+    return render_template("home.html", prediction = output,prediction_1=str(class0),prediction_2=str(class1),prediction_3=str(','.join(imp)))
 
 
 
